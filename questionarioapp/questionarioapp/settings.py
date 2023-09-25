ffa7644e1c1e28739e20dffce1820c9b7d490170
Arthur Sumny <arthur_sumny2000@hotmail.com>
2023-09-25 14:48:40 -0300

    teste

--- questionarioapp/questionarioapp/settings.py
+++ questionarioapp/questionarioapp/settings.py
@@ -125,7 +125,7 @@ CORS_ALLOWED_ORIGINS = [
 # Email
 
 DEFAULT_FROM_EMAIL = 'arthursmuny@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
